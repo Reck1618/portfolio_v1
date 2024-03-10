371dcf0e0f2b2f59e71dbcaac59fd43aaf753c4a
Ritik Tyagi <ritiktyagi1.618@gmail.com>
2024-03-10 20:48:55 +0530

    deciding which project card to keep

--- src/componenets/projects-card/Project-card.jsx
+++ src/componenets/projects-card/Project-card.jsx
@@ -1,25 +1,25 @@
+import { RxOpenInNewWindow } from "react-icons/rx";
+import { TbBrandGithub } from "react-icons/tb";
 import PropTypes from 'prop-types';
 import './Project-card.css';
 
 
 const ProjectCard = ({ project }) => {
 
-    const {name, image, description, url, skills} = project;
+    const {name, image, description, live, url, skills} = project;
 
     return (
         <div className='project-card'>
-            {/* <div className='project-image'>
-                <img src={image} alt={name} />
-            </div> */}
+            <img className='project-image' src={image} alt={name} />
 
             <div className='project-content'>
-                <div className='project-heading'>
-                    <h3>{name}</h3>
+                <div className='project-links'>
+                    <a href={url} target="_blank" rel="noreferrer"><TbBrandGithub /></a>
+                    <a href={live} target="_blank" rel="noreferrer"><RxOpenInNewWindow /></a>
                 </div>
 
-                <div className='project-links'>
-                    <a href={url} target="_blank" rel="noreferrer">View Project</a>
-                    <a href={url} target="_blank" rel="noreferrer">View Project</a>
+                <div className='project-heading'>
+                    <h3>{name}</h3>
                 </div>
 
                 <div className='project-description'>
@@ -42,6 +42,7 @@ ProjectCard.propTypes = {
         name: PropTypes.string.isRequired,
         image: PropTypes.string.isRequired,
         description: PropTypes.string.isRequired,
+        live: PropTypes.string.isRequired,
         url: PropTypes.string.isRequired,
         skills: PropTypes.arrayOf(PropTypes.string).isRequired,
     }).isRequired,
