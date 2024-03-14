ce135bab2eab0669c4d23a73a59768629377dd9b
Micy <100793297+Miicy@users.noreply.github.com>
2024-03-14 14:17:19 +0100

    fixed experiance cards

--- src/componenets/experience/Experiance.jsx
+++ src/componenets/experience/Experiance.jsx
@@ -1,32 +1,36 @@
-import { experianceData } from '../../utils/data';
-import './Experiance.css'
+import { experianceData } from "../../utils/data";
+import "./Experiance.css";
 
 const Experiance = () => {
+	return (
+		<div className="experiance-container">
+			<ul className="experiance-list">
+				{experianceData.map((experiance, index) => {
+					return (
+						<li key={index}>
+							<div className="content">
+								<span className="experiance-content">{experiance.title}</span>
 
-    return (
-        <div className='experiance-container'>
-             <ul className='experiance-list'>
-                {experianceData.map((experiance, index) => {
-                    return (
-                        <li key={index}>
-                            <div className="content">
-                                <span>{ experiance.title }</span>
-                                <span>{ experiance.company }</span>
-                                <span>{ experiance.duration }</span>
-                                <ul className='experiance-resp'>
-                                    {experiance.responsibilities.map((responsibility, index) => {
-                                        return (
-                                            <li className='experiance-resp' key={index}>{responsibility}</li>
-                                        )
-                                    })}
-                                </ul>
-                            </div>
-                        </li>
-                    )
-                })}
-            </ul>
-        </div>
-    );
+								<span className="experiance-company">
+									<div>{experiance.company}</div> 
+                                    <div>{experiance.duration}</div>
+								</span>
+								<ul className="experiance-resp">
+									{experiance.responsibilities.map((responsibility, index) => {
+										return (
+											<li className="experiance-resp" key={index}>
+												{responsibility}
+											</li>
+										);
+									})}
+								</ul>
+							</div>
+						</li>
+					);
+				})}
+			</ul>
+		</div>
+	);
 };
 
 export default Experiance;
